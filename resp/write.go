ab1d64a2d6a2373044f5a2043b255d9cf1d41fed
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-04-18 08:22:37 -0300

    Write method

--- resp/write.go
+++ resp/write.go
@@ -14,6 +14,17 @@ func NewWriter(w io.Writer) *Writer {
 	return &Writer{writer: w}
 }
 
+func (w *Writer) Write(v Value) error {
+	var bytes = v.Marshal()
+
+	_, err := w.writer.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (v Value) Marshal() []byte {
 	switch v.typ {
 	case Array:
